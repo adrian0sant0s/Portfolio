731fdc7875a0ff4012747e0fdfdb0e4738902c8d
Adriano <adrianosantosr1@gmail.com>
2022-10-27 12:44:56 -0300

    adicionando novo projeto

--- src/containers/project/index.js
+++ src/containers/project/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import Carousel from 'react-elastic-carousel'
+import Carousel from "react-elastic-carousel";
 
 import { BiLeftArrow } from "react-icons/bi";
 
@@ -9,6 +9,7 @@ import ImgBurguer from "../../assets/codeBurguer.jpg";
 import LojaDeSom from "../../assets/lojaDeSom.png";
 import Spotfy from "../../assets/Spotfy.png";
 import Aéreo from "../../assets/Aéreo-barato.png";
+import ToDoList from "../../assets/toDoList.png";
 
 import {
   Navbar,
@@ -17,7 +18,6 @@ import {
   Project,
   Header,
   Item,
- 
   Image,
 } from "./styles";
 
@@ -33,8 +33,8 @@ function Projects() {
     { width: 400, itemsToShow: 2 },
     { width: 600, itemsToShow: 3 },
     { width: 900, itemsToShow: 4 },
-    { width: 1300, itemsToShow: 5 }
-]
+    { width: 1300, itemsToShow: 5 },
+  ];
 
   return (
     <>
@@ -49,54 +49,74 @@ function Projects() {
       </Navbar>
 
       <Header>
-        <Carousel itemsToShow={1}
-                style={{ width: '90%'}}
-                breakPoints={breakPoints}>
-          <Item >
-              <p>Conversor de moedas</p>
-              <Image src={ImgConversor} alt="foto do conversor" />
-              <a href="https://conversor-de-moedas-adrianosantos.netlify.app/" target='blank'>
-                  <input type="button" value="Click para saber mais"/>
-
-              </a>
+        <Carousel
+          itemsToShow={1}
+          style={{ width: "90%" }}
+          breakPoints={breakPoints}
+        >
+          <Item>
+            <p>To do list</p>
+            <Image src={ToDoList} alt="foto to do list" />
+            <a
+              href="https://peaceful-kitten-9855ae.netlify.app/"
+              target="blank"
+            >
+              <input type="button" value="Click para saber mais" />
+            </a>
           </Item>
 
           <Item>
-          <p>Cadastro de pedidos</p>
-          <Image src={ImgBurguer} alt="foto da hamburgueria" />
-          <a href="https://github.com/adrian0sant0s/cadastro-de-pedidos-lanchonete-FrontEnd" target='blank'>
-                  <input type="button" value="Click para saber mais"/>
-              </a>
-          </Item>  
+            <p>Conversor de moedas</p>
+            <Image src={ImgConversor} alt="foto do conversor" />
+            <a
+              href="https://conversor-de-moedas-adrianosantos.netlify.app/"
+              target="blank"
+            >
+              <input type="button" value="Click para saber mais" />
+            </a>
+          </Item>
 
           <Item>
-          <p>Loja de som</p>
+            <p>Cadastro de pedidos</p>
+            <Image src={ImgBurguer} alt="foto da hamburgueria" />
+            <a
+              href="https://github.com/adrian0sant0s/cadastro-de-pedidos-lanchonete-FrontEnd"
+              target="blank"
+            >
+              <input type="button" value="Click para saber mais" />
+            </a>
+          </Item>
+
+          <Item>
+            <p>Loja de som</p>
             <Image src={LojaDeSom} alt="foto da loja de som" />
-            <a href="https://loja-de-som-automotivo.netlify.app/" target='blank'>
-                  <input type="button" value="Click para saber mais"/>
-              </a>
-          </Item>  
+            <a
+              href="https://loja-de-som-automotivo.netlify.app/"
+              target="blank"
+            >
+              <input type="button" value="Click para saber mais" />
+            </a>
+          </Item>
 
           <Item>
-          <p>Clone do spotfy</p>  
+            <p>Clone do spotfy</p>
             <Image src={Spotfy} alt="foto do clone do spotfy" />
-            <a href="https://clone-spotify-learn.netlify.app/" target='blank'>
-                  <input type="button" value="Click para saber mais"/>
-              </a>
-          </Item>  
+            <a href="https://clone-spotify-learn.netlify.app/" target="blank">
+              <input type="button" value="Click para saber mais" />
+            </a>
+          </Item>
 
-        <Item>
-        <p>Página de passagens aéreas</p>
+          <Item>
+            <p>Página de passagens aéreas</p>
             <Image src={Aéreo} alt="foto da página de passagens aéreas" />
-            <a href="https://passagens-aereas-baratas.netlify.app/"  target='blank'>
-                  <input type="button" value="Click para saber mais"/>
-              </a>
-        </Item>     
-                
-
-          
+            <a
+              href="https://passagens-aereas-baratas.netlify.app/"
+              target="blank"
+            >
+              <input type="button" value="Click para saber mais" />
+            </a>
+          </Item>
         </Carousel>
-       
       </Header>
     </>
   );
